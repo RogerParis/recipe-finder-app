2b91f5dc0dcead43e3e153c8f1bab7673141da63
Roger Paris <roger.parisvicent@gmail.com>
2025-03-08 16:46:54 +0100

    adds env conversion file

---
+++ config/env.tsx
@@ -0,0 +1,13 @@
+import Constants from "expo-constants";
+
+const ENV = process.env.EXPO_PUBLIC_ENV || "dev";
+
+const getEnvVars = () => {
+  const extra = Constants.expoConfig?.extra;
+  return {
+    API_URL: extra?.API_URL[ENV] || extra?.API_URL["dev"],
+    OPENAI_API_KEY: extra?.OPENAI_API_KEY[ENV] || extra?.OPENAI_API_KEY["dev"],
+  };
+};
+
+export const env = getEnvVars();
