5ed49981004851c48f7a5df78fc58cfd541ea14b
Roger Paris <roger.parisvicent@gmail.com>
2025-03-19 00:56:37 +0100

    adds immer

--- store/useAISuggestions.ts
+++ store/useAISuggestions.ts
@@ -1,6 +1,7 @@
 import { env } from '@/config/env';
 import axios from 'axios';
 import { create } from 'zustand';
+import { immer } from 'zustand/middleware/immer';
 
 interface AISuggestionState {
   mealSuggestion: string;
@@ -9,39 +10,41 @@ interface AISuggestionState {
   fetchAISuggestion: () => Promise<void>;
 }
 
-export const useAISuggestions = create<AISuggestionState>((set) => ({
-  mealSuggestion: '',
-  error: null,
-  isLoading: true,
-  fetchAISuggestion: async () => {
-    set({ isLoading: true });
-    try {
-      set({ error: null });
-      const response = await axios.post(
-        'https://openrouter.ai/api/v1/chat/completions',
-        {
-          model: 'gpt-4',
-          messages: [
-            {
-              role: 'system',
-              content: 'Suggest an ingredient for a meal.',
-            },
-          ],
-          max_tokens: 100,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${env.AI_API_KEY}`,
-            'Content-Type': 'application/json',
+export const useAISuggestions = create<AISuggestionState>()(
+  immer((set) => ({
+    mealSuggestion: '',
+    error: null,
+    isLoading: true,
+    fetchAISuggestion: async () => {
+      set({ isLoading: true });
+      try {
+        set({ error: null });
+        const response = await axios.post(
+          'https://openrouter.ai/api/v1/chat/completions',
+          {
+            model: 'gpt-4',
+            messages: [
+              {
+                role: 'system',
+                content: 'Suggest an ingredient for a meal.',
+              },
+            ],
+            max_tokens: 100,
           },
-        },
-      );
-      set({ mealSuggestion: response.data.choices[0].message.content });
-    } catch (error) {
-      set({ error: 'Failed to fetch AI suggestion' });
-      console.error('Error fetching AI suggestion:', error);
-    } finally {
-      set({ isLoading: false });
-    }
-  },
-}));
+          {
+            headers: {
+              Authorization: `Bearer ${env.AI_API_KEY}`,
+              'Content-Type': 'application/json',
+            },
+          },
+        );
+        set({ mealSuggestion: response.data.choices[0].message.content });
+      } catch (error) {
+        set({ error: 'Failed to fetch AI suggestion' });
+        console.error('Error fetching AI suggestion:', error);
+      } finally {
+        set({ isLoading: false });
+      }
+    },
+  })),
+);
